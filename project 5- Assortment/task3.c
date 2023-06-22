84d733004efabe3d8c9612ccb932f9808026e981
moxa309 <134709566+moxa309@users.noreply.github.com>
2023-06-22 22:11:47 +0530

    Add files via upload

---
+++ project 5- Assortment/task3.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(){
+	
+	int n,m,i,j;
+	
+	printf("Enter the size of rows : ");
+	scanf("%d",&n);
+	
+	printf("Enter the size of column : ");
+	scanf("%d",&m);
+	
+	int a[n][m];
+	
+	for(i=0;i<n;i++){
+		
+		for(j=0;j<m;j++){
+			
+			printf("Enter the value of matrix : ");
+			scanf("%d",&a[i][j]);
+		}
+	}
+	
+	for(i=0;i<n;i++){
+		
+		for(j=0;j<m;j++){
+			
+			printf("%d",a[i][j]);
+			
+		}
+		printf("\n");
+	}
+	printf("Rotated matrix : ");
+	printf("\n");
+	
+	for(i=0;i<n;i++){
+		
+		for(j=2;j>=0;j--){
+			
+			printf("%d",a[i][j]);
+		
+		}
+		printf("\n");
+	}
+		
+	return 0;
+}
